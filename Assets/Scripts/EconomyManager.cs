fc63a1a7f59e17eb48319c485605571d828db980
ErdelyiAndras <andraserdelyi17@gmail.com>
2025-03-13 12:22:26 +0100

    created game difficulty enum and implemented base of economy manager

--- Assets/Scripts/EconomyManager.cs
+++ Assets/Scripts/EconomyManager.cs
@@ -7,42 +7,79 @@ using System.Threading.Tasks;
 
 public class EconomyManager
 {
+    public readonly int easyGameStartMoney = 700;
+    public readonly int mediumGameStartMoney = 500;
+    public readonly int hardGameStartMoney = 300;
+
     private int money;
 
-    public Action<bool> GameOver;
+    private bool wasInitialized = false;
 
-    private readonly int easyGameStartMoney = 700;
-    private readonly int mediumGameStartMoney = 500;
-    private readonly int hardGameStartMoney = 300;
-    public int Money 
+    public int Money
     {
-        get { return money; }
-    }
-    public bool SpendMoney(int amount)
-    {
-        if (money < amount)
+        get
         {
-            return false;
+            return money;
         }
+    }
+
+    public Action GoneBankrupt;
+    
+    // TODO: balance costs
+    public int UnitCostOfPlant => 20; 
+    public int UnitCostOfHerbivore => 50;
+    public int UnitCostOfCarnivore => 100;
+    public int UnitCostOfJeep => 200;
+    public int UnitCostOfRoad => 10;
+    public int UnitCostOfLake => 100;
+
+    public void InitMoney(Difficulty difficulty)
+    {
+        if (wasInitialized)
+        {
+            throw new InvalidOperationException("EconomyManager was already initialized");
+        }
+        wasInitialized = true;
+        switch (difficulty)
+        {
+            case Difficulty.Easy:
+                money = easyGameStartMoney;
+                break;
+            case Difficulty.Medium:
+                money = mediumGameStartMoney;
+                break;
+            case Difficulty.Hard:
+                money = hardGameStartMoney;
+                break;
+        }
+    }
+
+    public bool HasEnoughMoney(int amount) => money >= amount;
+
+    public void SpendMoney(int amount)
+    {
         money -= amount;
         CheckIfGameOver();
-        return true;
+    }
+
+    public void EarnMoney(int amount)
+    {
+        money += amount;
     }
 
     private void CheckIfGameOver()
     {
-        if (IsPlayerBankrupt())
+        if (IsBankrupt())
         {
-            OnGameOver(false);
+            OnGoneBankrupt();
         }
-        //TODO 
     }
 
-    private bool IsPlayerBankrupt() => money > 0;
+    private bool IsBankrupt() => money < 0;
 
-    private void OnGameOver(bool result)
+    private void OnGoneBankrupt()
     {
-        GameOver?.Invoke(result);
+        GoneBankrupt?.Invoke();
     }
 
 }
