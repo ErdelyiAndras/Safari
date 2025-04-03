d8ae5804692e66cfb6ed641b101377f050ffbfc4
ErdelyiAndras <andraserdelyi17@gmail.com>
2025-04-03 18:11:27 +0200

    created MainMenu scene and added buttons to it

---
+++ Assets/Scripts/UI/MainMenuUIController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUIController : MonoBehaviour
+{
+    public Toggle easyToggle, normalToggle, hardToggle;
+    public Difficulty selectedDifficulty;
+
+    private void Start()
+    {
+        
+    }
+}
