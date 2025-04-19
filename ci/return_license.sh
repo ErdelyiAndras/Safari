a70a72bd394c847b831050fd89b25518e9465ca8
ErdelyiAndras <andraserdelyi17@gmail.com>
2025-04-20 00:32:33 +0200

    added build for linux

--- ci/return_license.sh
+++ ci/return_license.sh
@@ -29,25 +29,25 @@ elif [[ -n "${UNITY_SERIAL:-}" ]]; then
     exit 1
   fi
 
-  # unity-editor \
-  #   -logFile /dev/stdout \
-  #   -quit \
-  #   -returnlicense \
-  #   -username "$UNITY_EMAIL" \
-  #   -password "$UNITY_PASSWORD" \
-  #   -projectPath "$project_path" || {
-  #     echo "Error: Failed to return serial license" >&2
-  #     exit 1
-  #   }
   unity-editor \
     -logFile /dev/stdout \
     -quit \
     -returnlicense \
     -username "$UNITY_EMAIL" \
-    -password "$UNITY_PASSWORD" || {
+    -password "$UNITY_PASSWORD" \
+    -projectPath "$project_path" || {
       echo "Error: Failed to return serial license" >&2
       exit 1
     }
+  # unity-editor \
+  #   -logFile /dev/stdout \
+  #   -quit \
+  #   -returnlicense \
+  #   -username "$UNITY_EMAIL" \
+  #   -password "$UNITY_PASSWORD" || {
+  #     echo "Error: Failed to return serial license" >&2
+  #     exit 1
+  #   }
 else
   echo "Error: Neither UNITY_LICENSING_SERVER nor UNITY_SERIAL is set" >&2
   exit 1
